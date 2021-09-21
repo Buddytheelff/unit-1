8709ac0d7c2df6379bf186359939074496a5b44e
Buddy Collins <50601293+Buddytheelff@users.noreply.github.com>
2021-09-21 13:18:03 -0600

    Activity 5

--- js/adaptedTutorial.js
+++ js/adaptedTutorial.js
@@ -1,9 +1,18 @@
-// Author:Buddy Collins
+//Author Buddy Collins
 
-// I liked this map better than the gray one
+// I liked this map better than the gray one 
 
-var map = L.map('mapid').setView([40.7393024, -73.9652925], 11);
-L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
+var map = L.map('mapid').setView([20, 0], 2);
+
+
+L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+  attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+  maxZoom: 18,
+  id: 'mapbox/streets-v11',
+  tileSize: 512,
+  zoomOffset: -1,
+  accessToken: 'pk.eyJ1IjoiYnVkZHl0aGVlbGYiLCJhIjoiY2p2czYxbHNlMndjdzN5cGI3ejI4aWJ5dyJ9.5IPoN9dD2-6boYGfX4ondQ'
+}).addTo(map);
 
 //-----------------------------------------------------------------------------------------------------------------------
 // addes the popups to each Feature
@@ -11,7 +20,7 @@ function onEachFeature(feature, layer) {
     //no property named popupContent; instead, create html string with all properties
     var popupContent = "";
     if (feature.properties) {
-        //loop to add all feature property names and values to html string not just the ones you want all
+        //loop to add feature property names and values to html string
         for (var property in feature.properties){
             popupContent += "<p>" + property + ": " + feature.properties[property] + "</p>";
         }
@@ -19,14 +28,15 @@ function onEachFeature(feature, layer) {
     };
 };
 
-$.getJSON("data/pickup_sample.geojson", function(response){
+$.getJSON("data/MegaCities.geojson", function(response){
+  //create a Leaflet GeoJSON layer and add it to the map
   var geojsonMarkerOptions = {
     radius: 8,
-    fillColor: "blue",
+    fillColor: "#ff7800",
     color: "#000",
     weight: 1,
     opacity: 1,
-    fillOpacity: 0.5
+    fillOpacity: 0.8
   };
   L.geoJson(response, {
     pointToLayer: function (feature, latlng){
---
+++ js/geojsonTutorial.js
@@ -0,0 +1,145 @@
+//Author Buddy Collins
+
+var map = L.map('mapid').setView([39.7536634, -104.9678022], 13);
+
+
+L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+  attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+  maxZoom: 18,
+  id: 'mapbox/streets-v11',
+  tileSize: 512,
+  zoomOffset: -1,
+  accessToken: 'pk.eyJ1IjoiYnVkZHl0aGVlbGYiLCJhIjoiY2p2czYxbHNlMndjdzN5cGI3ejI4aWJ5dyJ9.5IPoN9dD2-6boYGfX4ondQ'
+}).addTo(map);
+
+
+function onEachFeature(feature, layer) {
+    // does this feature have a property named popupContent? yes it does
+    if (feature.properties && feature.properties.popupContent) {
+        layer.bindPopup(feature.properties.popupContent);
+    }
+}
+
+var geojsonFeature = {
+    "type": "Feature",
+    "properties": {
+        "name": "Coors Field",
+        "amenity": "Baseball Stadium",
+        "popupContent": "This is where the Rockies play!"
+    },
+    "geometry": {
+        "type": "Point",
+        "coordinates": [-104.99404, 39.75621]
+    }
+};
+
+
+L.geoJSON(geojsonFeature, {
+    onEachFeature: onEachFeature
+}).addTo(map); // adds the point to the map with the popup call
+
+//------------------------------------------------------------------------------------------------------------------------
+//adds the lines
+var myLines = [{
+    "type": "LineString",
+    "coordinates": [[-100, 40], [-105, 45], [-110, 55]]
+}, {
+    "type": "LineString",
+    "coordinates": [[-105, 40], [-110, 45], [-115, 55]]
+}];
+
+var myStyle = {
+    "color": "#ff7800",
+    "weight": 5,
+    "opacity": 0.65
+};
+//adds the lines with style
+L.geoJSON(myLines, {
+    style: myStyle
+}).addTo(map);
+//------------------------------------------------------------------------------------------------------------------------
+
+// adds the states to the maps
+var states = [{
+    "type": "Feature",
+    "properties": {"party": "Republican"},
+    "geometry": {
+        "type": "Polygon",
+        "coordinates": [[
+            [-104.05, 48.99],
+            [-97.22,  48.98],
+            [-96.58,  45.94],
+            [-104.03, 45.94],
+            [-104.05, 48.99]
+        ]]
+    }
+}, {
+    "type": "Feature",
+    "properties": {"party": "Democrat"},
+    "geometry": {
+        "type": "Polygon",
+        "coordinates": [[
+            [-109.05, 41.00],
+            [-102.06, 40.99],
+            [-102.03, 36.99],
+            [-109.04, 36.99],
+            [-109.05, 41.00]
+        ]]
+    }
+}];
+
+L.geoJSON(states, {
+    style: function(feature) {
+        switch (feature.properties.party) {
+            case 'Republican': return {color: "#ff0000"};
+            case 'Democrat':   return {color: "#0000ff"};
+        }
+    }
+}).addTo(map);
+//------------------------------------------------------------------------------------------------------------------------
+
+// adds the circle to the map or a circle i just used the point data from the previous example
+var geojsonMarkerOptions = {
+    radius: 8,
+    fillColor: "#ff7800",
+    color: "#000",
+    weight: 1,
+    opacity: 1,
+    fillOpacity: 0.8
+};
+
+L.geoJSON(geojsonFeature, {
+    pointToLayer: function (feature, latlng) {
+        return L.circleMarker(latlng, geojsonMarkerOptions);
+    }
+}).addTo(map);
+//------------------------------------------------------------------------------------------------------------------------
+
+// filter to keep that random point out of here
+var someFeatures = [{
+    "type": "Feature",
+    "properties": {
+        "name": "Coors Field",
+        "show_on_map": false // set to false to prevent the issues with overlap on coors field (it doesnt allow the popup)
+    },
+    "geometry": {
+        "type": "Point",
+        "coordinates": [-104.99404, 39.75621]
+    }
+}, {
+    "type": "Feature",
+    "properties": {
+        "name": "Busch Field",
+        "show_on_map": false // determining factor in the filter
+    },
+    "geometry": {
+        "type": "Point",
+        "coordinates": [-104.98404, 39.74621]
+    }
+}];
+
+L.geoJSON(someFeatures, {
+    filter: function(feature, layer) {
+        return feature.properties.show_on_map;
+    }
+}).addTo(map);
